836d0ba5c288e297f75919e6ca0247712864c12c
cacho <394369786@qq.com>
2017-11-18 15:50:46 +0800

    add VersionControl folder

--- AddRemote.bat
+++ AddRemote.bat
@@ -2,7 +2,7 @@
 
 cd C:\Users\Administrator.USER-20141106GE\PycharmProjects
 
-git remote add gitee https://gitee.com/Cacho/PycharmProjects.git
+git remote add gitee https://gitee.com/Cacho/VersionControl.git
 git remote add github https://github.com/Cachozeng/PycharmProjects.git
 
 git remote  -v
