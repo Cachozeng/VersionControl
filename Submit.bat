0a45f5d6c85b84097fa7011ad314095794ba78a7
cacho <394369786@qq.com>
2018-02-03 17:20:07 +0800

    add VersionControl folder

--- Submit.bat
+++ Submit.bat
@@ -3,11 +3,11 @@
 f:
 cd F:\SoftwareTest\Github\22 VersionControl
 
-git init
-git remote add gitee https://gitee.com/Cacho/VersionControl.git
-git remote add github https://github.com/Cachozeng/VersionControl.git
+REM git init
+REM git remote add gitee https://gitee.com/Cacho/VersionControl.git
+REM git remote add github https://github.com/Cachozeng/VersionControl.git
 
-git pull github master
+REM git pull github master
 git add .
 git commit -m "add VersionControl folder"
 git push gitee master
